a24bfd83ad2b8cf54ee2fcb75f6da8b487be01b6
Attila Füzes <attila.fuzes@cs.utcluj.ro>
2023-03-03 17:30:35 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
-#include "lab1.hh"
+// #include "lab1.hh"
 
-using Lab1 = utcn::ip::Lab1;
+// using Lab1 = utcn::ip::Lab1;
+
+#include "nfd.h"
+#include <cstdio>
+#include <cstdlib>
 
 int main() {
-  Lab1 lab1;
-  lab1.runLab();
+  // Lab1 lab1;
+  // lab1.runLab();
 
-  return 0;
+  // return 0;
+  NFD_Init();
+
+    nfdchar_t *outPath;
+    nfdfilteritem_t filterItem[2] = { { "Source code", "c,cpp,cc" }, { "Headers", "h,hpp" } };
+    nfdresult_t result = NFD_OpenDialog(&outPath, filterItem, 2, NULL);
+    if (result == NFD_OKAY)
+    {
+        puts("Success!");
+        puts(outPath);
+        NFD_FreePath(outPath);
+    }
+    else if (result == NFD_CANCEL)
+    {
+        puts("User pressed cancel.");
+    }
+    else 
+    {
+        printf("Error: %s\n", NFD_GetError());
+    }
+
+    NFD_Quit();
+    return 0;
 }
